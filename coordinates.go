345de62e3d24347e3f07e466ab0ea27eeb959536
Stella Xu <stellaxu3116@gmail.com>
2020-07-16 10:45:36 -0400

    I finished the setter methods

--- coordinates.go
+++ coordinates.go
@@ -1,5 +1,23 @@
 package coordinates
 
-type Event struct {
+import "errors"
+
+type Coordinates struct {
 	longitude, latitude float64
+}
+
+//setter methods:
+func (c *Coordinates) SetLongitude(lon float64) error {
+	if lon < -90 || lon > 90 {
+		return errors.New("Invalid longitude")
+	}
+	c.longitude = lon
+	return nil
+}
+func (c *Coordinates) SetLatitude(lat float64) error {
+	if lat < -90 || lat > 90 {
+		return errors.New("Invalid latitude")
+	}
+	c.latitude = lat
+	return nil
 }
